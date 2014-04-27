bc970e439c38020e3db46ede4631dd1f8f5fc05d
Li-Wei Tseng <li-weitseng@s-128-97-75-59.resnet.ucla.edu>
2014-04-26 18:37:09 -0700

    Initial Commit

---
+++ HW 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HW 2
+//
+//  Created by Li-Wei Tseng on 4/26/14.
+//  Copyright (c) 2014 Li-Wei Tseng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
